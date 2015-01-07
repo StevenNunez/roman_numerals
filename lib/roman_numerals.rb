4c1970032519e7f7d04ecc863be1a91f3b6df4f8
Steven Nunez <steven.nunez@gmail.com>
2015-01-07 17:55:53 -0500

    Addition and subtraction

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -2,6 +2,37 @@ require "roman_numerals/version"
 
 class RomanNumeral
   attr_reader :characters
+  def initialize(characters)
+    @characters = characters
+  end
+
+  def to_i
+    tokenize(characters).inject(0, &roman_arithmatic)
+  end
+
+  private
+
+  def roman_arithmatic
+    lambda do |total, slice|
+      if slice.size > 1
+        first, second = *RomanCharacter.values_for(slice)
+        total + (second - first)
+      else
+        total + RomanCharacter.value_for(slice.first)
+      end
+    end
+  end
+
+  def tokenize(characters)
+    characters.chars.slice_when do |before, after|
+      first = RomanCharacter.value_for(before)
+      second = RomanCharacter.value_for(after)
+      first >= second
+    end
+  end
+end
+
+class RomanCharacter
   I = 1
   V = 5
   X = 10
@@ -10,11 +41,13 @@ class RomanNumeral
   D = 500
   M = 1000
 
-  def initialize(characters)
-    @characters = characters
+  def self.value_for(character)
+    Object.const_get("#{self}::#{character}")
   end
 
-  def to_i
-    Object.const_get("#{self.class}::#{characters}")
+  def self.values_for(characters)
+    characters.map do |character|
+      Object.const_get("#{self}::#{character}")
+    end
   end
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,22 +1,36 @@
 require 'spec_helper'
 
-  describe RomanNumerals do
-    # IV = 4
-    # XXXIV = 34
-    # CCLXVII = 267
-    # DCCLXIV = 764
-    # CMLXXXVII = 987
-    # MCMLXXXIII = 1983
-    # MMXIV = 2014
-    # MMMM = 4000
-    # MMMMCMXCIX = 4999
-    it "converts single letters to decimal equivalent" do
-      expect(RomanNumeral.new('I').to_i).to eq(1)
-      expect(RomanNumeral.new('V').to_i).to eq(5)
-      expect(RomanNumeral.new('X').to_i).to eq(10)
-      expect(RomanNumeral.new('L').to_i).to eq(50)
-      expect(RomanNumeral.new('C').to_i).to eq(100)
-      expect(RomanNumeral.new('D').to_i).to eq(500)
-      expect(RomanNumeral.new('M').to_i).to eq(1000)
-    end
+describe RomanNumerals do
+  # IV = 4
+  # XXXIV = 34
+  # CCLXVII = 267
+  # DCCLXIV = 764
+  # CMLXXXVII = 987
+  # MCMLXXXIII = 1983
+  # MMXIV = 2014
+  # MMMM = 4000
+  # MMMMCMXCIX = 4999
+  it "converts single letters to decimal equivalent" do
+    expect(RomanNumeral.new('I').to_i).to eq(1)
+    expect(RomanNumeral.new('V').to_i).to eq(5)
+    expect(RomanNumeral.new('X').to_i).to eq(10)
+    expect(RomanNumeral.new('L').to_i).to eq(50)
+    expect(RomanNumeral.new('C').to_i).to eq(100)
+    expect(RomanNumeral.new('D').to_i).to eq(500)
+    expect(RomanNumeral.new('M').to_i).to eq(1000)
   end
+
+  it "does additive math" do
+    expect(RomanNumeral.new('II').to_i).to eq(2)
+    expect(RomanNumeral.new('MDCLXVI').to_i).to eq(1666)
+  end
+
+  it "does subtractive math" do
+    expect(RomanNumeral.new('IV').to_i).to eq(4)
+    expect(RomanNumeral.new('IX').to_i).to eq(9)
+    expect(RomanNumeral.new('XL').to_i).to eq(40)
+    expect(RomanNumeral.new('XC').to_i).to eq(90)
+    expect(RomanNumeral.new('CD').to_i).to eq(400)
+    expect(RomanNumeral.new('CM').to_i).to eq(900)
+  end
+end
